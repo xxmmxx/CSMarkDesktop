29b260fc903c76ba5d6e9a19c11ca9456ea6e71f
AluminiumTech <alastairlundy888@gmail.com>
2018-05-13 23:52:56 +0100

    Moved StressTesting code to StressTest app

--- src/CSMark/Program.cs
+++ src/CSMark/Program.cs
@@ -11,87 +11,9 @@ using System.IO;
 
 namespace CSMark {
     class Program {
-        enum Command {
-            StressTest,
-            StressTest_AndStartTime,
-            StressTest_AndStartAndEndTime,
-        }
+      
         static void Main(string[] args){
-            Command command = Command.StressTest;
-            DateTime startTime = DateTime.Now;
-            DateTime stopTime = DateTime.Now;
-            StressTestController stc = new StressTestController();
-
-            //Accept command line arguments
-            if (args.Length == 0){
-                //If the user doesn't specify the command, just start the stress test.
-                command = Command.StressTest;
-            }
-            // If the user has given 1 arguments they should correspond to 1) Start Time.
-            else if (args.Length == 1){
-                command = Command.StressTest_AndStartTime;
-                startTime = Convert.ToDateTime(args[0]);
-            }
-            //If the user has given 2 arguments they should correspond to 1) Start Time and 2) End Time.
-            else if (args.Length == 2){
-                command = Command.StressTest_AndStartAndEndTime;
-                startTime = Convert.ToDateTime(args[0]);
-                stopTime = Convert.ToDateTime(args[1]);
-            }
-
-            Platform platform = new Platform();
-            string CSMarkVersion = platform.ReturnVersionString() + "_";
-            BenchmarkController bench = new BenchmarkController();   
-
-            //Show license information
-            platform.ShowLicenseInConsole("LicenseMessage.txt", 3000);
-
-            //Setup Rollbar Error Detection.
-            //You will need to create your own file in this directory called "Rollbar.txt" with a Rollbar API key to use Rollbar error reporting.
-            try{
-                string postServerItemAccessToken = File.ReadAllText("Rollbar.txt");
-                RollbarLocator.RollbarInstance.Configure(new RollbarConfig(postServerItemAccessToken) { Environment = "production" });
-            }
-            catch{
-                Console.WriteLine("We were unable to setup Rollbar Error Reporting.");
-            }
-
-            string shutdownNotice = "Don't turn off your PC.";
-
-            Console.Title = "CSMarkDesktop " + platform.ReturnVersionString() + " Community Edition";
-            Information.ConsoleWriteLineUsingOSColor("Welcome to CSMark Desktop Edition.");
-
-            //Warn the user if the process count is quite high.
-            platform.WarnProcessCount(200);
-
-            Console.WriteLine("                                                                        ");
-            Console.WriteLine("By using CSMarkDesktop, you agree to our Privacy Policy located at");
-            Console.WriteLine(" https://github.com/CSMarkBenchmark/CSMarkDesktop/blob/master/PrivacyPolicy.md");
-            Console.WriteLine("                                                                        ");
-            Console.WriteLine(shutdownNotice);
-            Console.WriteLine("                                                                        ");
-
-            Console.WriteLine("Detecting CLI Arguments...");
-            Console.WriteLine("Starting Stress Test...");
-
-            if(command == Command.StressTest){
-                //Start the Stress Test
-                 stc.StartMultiStressTest();
-            }
-            else if (command == Command.StressTest_AndStartTime){
-                //Start the stress test at the start time.
-                stc.StartMultiStressTest(startTime);
-            }
-            else if(command == Command.StressTest_AndStartAndEndTime){
-                //Start the stress test at the start time.
-                stc.StartMultiStressTest(startTime);
-                //Stop the stress test at the stop time.
-                stc.stopStressTest(stopTime);
-            }
-
-            Console.WriteLine("To exit this application, press ENTER.");
-            Console.ReadLine();
-            stc.StopStressTest();
+            
         }
   }
 }
--- src/CSMarkDesktopStressTestApp/Program.cs
+++ src/CSMarkDesktopStressTestApp/Program.cs
@@ -11,13 +11,39 @@ using System.IO;
 
 namespace CSMarkDesktopStressTestApp{
     class Program{
+        enum Command{
+            StressTest,
+            StressTest_AndStartTime,
+            StressTest_AndStartAndEndTime,
+        }
         static void Main(string[] args){
-            /// Accept command line arguments here.
-
             Platform platform = new Platform();
-            Console.Title = "CSMarkDesktop Stress Test App v" + platform.ReturnVersionString();
-            
-            StressTestController stress = new StressTestController();
+            Console.Title = "CSMarkDesktop StressTest App v" + platform.ReturnVersionString() + " Community Edition";
+
+           Command command = Command.StressTest;
+            DateTime startTime = DateTime.Now;
+            DateTime stopTime = DateTime.Now;
+            StressTestController stc = new StressTestController();
+
+            //Accept command line arguments
+            if (args.Length == 0){
+                //If the user doesn't specify the command, just start the stress test.
+                command = Command.StressTest;
+            }
+            // If the user has given 1 arguments they should correspond to 1) Start Time.
+            else if (args.Length == 1){
+                command = Command.StressTest_AndStartTime;
+                startTime = Convert.ToDateTime(args[0]);
+            }
+            //If the user has given 2 arguments they should correspond to 1) Start Time and 2) End Time.
+            else if (args.Length == 2){
+                command = Command.StressTest_AndStartAndEndTime;
+                startTime = Convert.ToDateTime(args[0]);
+                stopTime = Convert.ToDateTime(args[1]);
+            }
+
+            string CSMarkVersion = platform.ReturnVersionString() + "_";
+            BenchmarkController bench = new BenchmarkController();   
 
             //Show license information
             platform.ShowLicenseInConsole("LicenseMessage.txt", 3000);
@@ -32,16 +58,39 @@ namespace CSMarkDesktopStressTestApp{
                 Console.WriteLine("We were unable to setup Rollbar Error Reporting.");
             }
 
+            string shutdownNotice = "Don't turn off your PC.";
+
+            //Warn the user if the process count is quite high.
+            platform.WarnProcessCount(200);
+
             Console.WriteLine("                                                                        ");
-            Console.WriteLine("By using CSMarkDesktop, you agree to our Privacy Policy located");
-            Console.WriteLine(" at https://github.com/CSMarkBenchmark/CSMarkDesktop/blob/master/PrivacyPolicy.md");
+            Console.WriteLine("By using CSMarkDesktop, you agree to our Privacy Policy located at");
+            Console.WriteLine(" https://github.com/CSMarkBenchmark/CSMarkDesktop/blob/master/PrivacyPolicy.md");
+            Console.WriteLine("                                                                        ");
+            Console.WriteLine(shutdownNotice);
             Console.WriteLine("                                                                        ");
 
-            Console.WriteLine("Starting Stress Test. To terminate the stress test, please exit this application window or press enter.");
+            Console.WriteLine("Detecting CLI Arguments...");
+            Console.WriteLine("Starting Stress Test...");
 
-            stress.StartMultiStressTest();
+            if(command == Command.StressTest){
+                //Start the Stress Test
+                 stc.StartMultiStressTest();
+            }
+            else if (command == Command.StressTest_AndStartTime){
+                //Start the stress test at the start time.
+                stc.StartMultiStressTest(startTime);
+            }
+            else if(command == Command.StressTest_AndStartAndEndTime){
+                //Start the stress test at the start time.
+                stc.StartMultiStressTest(startTime);
+                //Stop the stress test at the stop time.
+                stc.stopStressTest(stopTime);
+            }
 
+            Console.WriteLine("To exit this application, press ENTER.");
             Console.ReadLine();
+            stc.StopStressTest();
         }
     }
 }
