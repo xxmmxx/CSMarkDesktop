4a964774c369d00b8444565aa24c9a52fc8ee8df
AluminiumTech <aluminiumdev@gmail.com>
2018-05-21 13:56:57 +0100

    Cleaned up folders and separated Stress Test CLI app from Benchmark App

---
+++ src/BenchmarkApp/Program.cs
@@ -0,0 +1,17 @@
+﻿/*  Copyright 2017-2018 AluminiumTech
+   This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+  */
+using AluminiumCoreLib.Utilities;
+using System;
+
+namespace BenchmarkApp{
+    class Program{
+        static void Main(string[] args)
+        {
+            Platform platform = new Platform();
+            Console.Title = "CSMarkDesktop BenchmarkApp v" + platform.ReturnVersionString() + " Community Edition";
+        }
+    }
+}
