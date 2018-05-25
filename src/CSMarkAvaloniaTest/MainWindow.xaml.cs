7c3ef0c375a5d873563f815ba62270fb4d1690a7
AluminiumTech <aluminiumdev@gmail.com>
2018-05-25 11:39:25 +0100

    started work on an Avalonia test app

---
+++ src/CSMarkAvaloniaTest/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+using Avalonia.Threading;
+using CSMarkLib;
+using System;
+
+namespace CSMarkAvaloniaTest
+{
+    public class MainWindow : Window
+    {
+       SolidColorBrush myGreenBrush = new SolidColorBrush(Color.FromRgb(125, 244, 66));
+        SolidColorBrush myRedBrush = new SolidColorBrush(Color.FromRgb(255, 0, 0));
+    
+        StressTestController stc;
+
+        DispatcherTimer t;
+        DateTime start;
+
+       private bool runningStress = false;
+
+        protected string betaURL = "https://raw.githubusercontent.com/CSMarkBenchmark/CSMarkDesktop/master/channels/wpf/beta.xml";
+        protected string stableURL = "https://raw.githubusercontent.com/CSMarkBenchmark/CSMarkDesktop/master/channels/wpf/stable.xml";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            stc = new StressTestController();
+            ApplyStressBtnColors();
+
+            //Show the version number
+        //    versionLabel.Content = "v" + Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+#if DEBUG
+            this.AttachDevTools();
+#endif
+        }
+
+        private void ApplyStressBtnColors()
+        {
+
+        }
+
+        private void HandleStressTest()
+        {
+
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
